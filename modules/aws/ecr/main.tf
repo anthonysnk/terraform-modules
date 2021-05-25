0d06bca4bb07c291547d43f08cfa8a83baa8ea6f
azepedaas <azepeda@applaudostudios.com>
2021-05-25 10:45:20 -0600

    Added ECR policy

--- modules/aws/ecr/main.tf
+++ modules/aws/ecr/main.tf
@@ -2,7 +2,33 @@
 # ECR Module
 # -------------------------------------------------------------
 
-## ECR repo
+## ECR repo policy
+resource "aws_ecr_lifecycle_policy" "appl-expire-policy" {
+  for_each             = length(local.repos) > 0 ? local.repos : {}
+
+  repository = aws_ecr_repository.repo.name
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 20 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 20
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+## ECR repo 
 resource "aws_ecr_repository" "repo" {
   for_each             = length(local.repos) > 0 ? local.repos : {}
   name                 = each.value.name
